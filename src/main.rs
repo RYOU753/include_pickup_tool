eedf9653b1585fd7aa96326af1fd3e551f2c0931
RYOU753 <ry75310@gmail.com>
2023-05-18 11:41:14 +0900

    コマンド形式の実行方法に変更

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct Args {
 	class_dirs:Vec<PathBuf>,
 	/// インクルードするフォルダを作る場所
 	#[arg(short,long,value_name = "PATH")]
-	include_path: Option<PathBuf>,
+	include_make_dir_path: Option<PathBuf>,
 	/// ヘッダファイルを一つにまとめる
 	#[arg(short,long,default_value_t = false)]
 	all_pack:bool,
@@ -25,50 +25,58 @@ struct Args {
 
 fn main() {
 	//.hのがあるフォルダの絶対パス(or相対パス)を取得
-	let args = Args::parse();
+	let mut args = Args::parse();
 	println!("{:?}",args);
 
 	//exeファイルの絶対パスを取得
-	let exe_pathbuf = env::current_dir().unwrap_or_else(|err| {
+	let exe_dir_pathbuf = env::current_dir().unwrap_or_else(|err| {
 		println!("exe not found path {err}");
 		std::process::exit(1);
 	});
+	println!("{}",exe_dir_pathbuf.display());
 	//exeと同じ階層にIncludeフォルダを作成
 	const INCLUDE_DIR_NAME:&str = "Include";
-	match fs::create_dir(args.include_path.clone().unwrap_or(Path::new(INCLUDE_DIR_NAME).to_path_buf())){
-		Ok(()) => {
-			println!("succes create Include dir");
-		},
-		Err(e) => {
-			println!("failed create Include dir {e}");
-			std::process::exit(1);
-		},
-	};
-	let include_path = match args.include_path{
+	let include_dir_path = match args.include_make_dir_path{
 		Some(p) => p.join(INCLUDE_DIR_NAME),
-		None => exe_pathbuf.join(INCLUDE_DIR_NAME),
+		None => exe_dir_pathbuf.join(INCLUDE_DIR_NAME),
 	};
+	if !include_dir_path.is_dir(){
+		match fs::create_dir(&include_dir_path){
+			Ok(()) => {
+				println!("succes create Include dir");
+			},
+			Err(e) => {
+				println!("failed create Include dir {e}");
+				std::process::exit(1);
+			},
+		};
+	}
 	//最初からフォルダパスがあるか
-	/* let path_list:Vec<String>;
-	if !is_target(&args.class_dirs)
-	{
-		println!("入力");
-		path_list = get_target_path1();
-	}else{
-		path_list = args.class_dirs;
-	} */
+	if !args.class_dirs.iter().all(|i| i.is_dir()){
+		println!("class dir not existens");
+		std::process::exit(1);
+	}
+	//相対パスだった時絶対パスに変換
+	args.class_dirs.iter_mut().for_each(|f| 
+		if f.is_relative(){
+			*f = exe_dir_pathbuf.clone().join(&f).canonicalize().unwrap();
+			if f.to_string_lossy().to_string().starts_with("\\\\?\\"){
+				*f = PathBuf::from(f.to_string_lossy().to_string().replace("\\\\?\\", ""));
+			}
+		}
+	);
 	//ヘッダファイルの絶対パスリスト
 	let header_path_list = all_target(args.class_dirs.clone());
 	//Includeからみたヘッダファイルの相対パスリスト
 	let mut is_first = true;
 	for header_path in header_path_list.unwrap(){
-		let relative = header_path.strip_prefix(&exe_pathbuf).unwrap();
+		let relative = header_path.strip_prefix(&exe_dir_pathbuf).unwrap();
 		let relative_buf = path::Path::new("..").join(relative);
 		let header_file = relative.file_name().unwrap();
 		if args.all_pack {
-			create_all_header(&include_path.join("all_header.h"), relative_buf.to_string_lossy().to_string(),&mut is_first);
+			create_all_header(&include_dir_path.join("all_header.h"), relative_buf.to_string_lossy().to_string(),&mut is_first);
 		}else{
-			create_header(&include_path.join(header_file),relative_buf.to_string_lossy().to_string());
+			create_header(&include_dir_path.join(header_file),relative_buf.to_string_lossy().to_string());
 		}
 		println!("{}",relative_buf.display());
 	}
@@ -119,21 +127,8 @@ fn dir_h(files:&mut Vec<path::PathBuf>,p :&PathBuf)-> std::io::Result<()>{
 	return Ok(());
 }
 
-fn get_target_path1() -> Vec<String>{
-	let w = get_input();
-	let path = path::Path::new(&w);
-	println!("path_dir->({})",w);
-	println!("存在:{}",path.is_dir());
-
-	return vec![w];
-}
-
 fn get_input() -> String {
 	let mut input = String::new();
 	io::stdin().read_line(&mut input).ok();
 	return input.trim().to_string();
-}
-
-fn is_target(args: &Vec<String>) -> bool {
-	args.len() >= 2
 }
