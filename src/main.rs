37b3bc3ffbb72b8468c19b0b5d0f437d87af9e2f
RYOU753 <ry75310@gmail.com>
2023-06-08 07:58:11 +0900

    一部コード関数化 テスト入力を記入

--- src/main.rs
+++ src/main.rs
@@ -28,40 +28,33 @@ struct Args {
 
 fn main() {
 	//.hのがあるフォルダの絶対パス(or相対パス)を取得
-	let mut args = Args::parse();
-	println!("{:?}",args);
-
-	//exeファイルの絶対パスを取得
-	let exe_dir_pathbuf = env::current_dir().unwrap_or_else(|err| {
-		println!("exe not found path {err}");
-		std::process::exit(1);
-	});
-	println!("{}",exe_dir_pathbuf.display());
-	//exeと同じ階層にIncludeフォルダを作成
-	const INCLUDE_DIR_NAME:&str = "Include";
-	let include_dir_path = match args.include_make_dir_path{
-		Some(p) => p.join(INCLUDE_DIR_NAME),
-		None => exe_dir_pathbuf.join(INCLUDE_DIR_NAME),
+	//let mut args = Args::parse();
+	let mut args = Args {
+		class_dirs: vec![PathBuf::from("D:/source/Rust/include_pickup_tool/test/Class")],
+		include_make_dir_path: Option::from(PathBuf::from("test/Include")),
+		all_pack:false,
+		all_pack_name:String::from("all_header")
 	};
-	//すでにIncludeフォルダがある場合なにもしない
-	if !include_dir_path.is_dir(){
-		match fs::create_dir(&include_dir_path){
-			Ok(()) => {
-				println!("succes create Include dir");
-			},
-			Err(e) => {
-				println!("failed create Include dir {e}");
-				std::process::exit(1);
-			},
-		};
-	}
+	println!("{:?}",args);
+	let exe_dir_pathbuf = get_exe_dir();
+	
+}
+
+fn first_version(mut args: Args) {
+    //exeファイルの絶対パスを取得
+    let exe_dir_pathbuf = get_exe_dir();
+    println!("{}",exe_dir_pathbuf.display());
+    //指定したパスにInclude用のフォルダの決定(なにもパスがない場合はexeと同じ階層にIncludeフォルダのパスにする)
+	const INCLUDE_DIR_NAME:&str = "Include";
+    let include_dir_path = decided_include_dir(&args, &exe_dir_pathbuf);
+    //すでにInclude用のフォルダがある場合なにもしない
+	make_include_dir(&include_dir_path);
 	//最初からすべてのフォルダがあるか
 	if !args.class_dirs.iter().all(|i| i.is_dir()){
-		println!("class dir not existens");
-		std::process::exit(1);
+			println!("class dir not existens");
+			std::process::exit(1);
 	}
 	//Includeフォルダにclassフォルダの階層を作る
-	args.class_dirs.iter().for_each(|dir| fs::create_dir_all(include_dir_path.join(dir)));
 
 	//相対パスだった時絶対パスに変換
 	args.class_dirs.iter_mut().for_each(|f| 
@@ -78,18 +71,47 @@ fn main() {
 	//Includeからみたヘッダファイルの相対パスリスト
 	let mut is_first = true;
 	for header_path in header_path_list.unwrap(){
-		let relative = header_path.strip_prefix(&exe_dir_pathbuf).unwrap();
-		let relative_buf = path::Path::new("..").join(relative);
-		let header_file = relative.file_name().unwrap();
-		if args.all_pack {
-			create_all_header(&include_dir_path.join(args.all_pack_name.clone() + ".h"), relative_buf.to_string_lossy().to_string(),&mut is_first);
-		}else{
-			create_header(&include_dir_path.join(header_file),relative_buf.to_string_lossy().to_string());
+			let relative = header_path.strip_prefix(&exe_dir_pathbuf).unwrap();
+			let relative_buf = path::Path::new("..").join(relative);
+			let header_file = relative.file_name().unwrap();
+			if args.all_pack {
+				create_all_header(&include_dir_path.join(args.all_pack_name.clone() + ".h"), relative_buf.to_string_lossy().to_string(),&mut is_first);
+			}else{
+				create_header(&include_dir_path.join(header_file),relative_buf.to_string_lossy().to_string());
+			}
+			println!("{}",relative_buf.display());
 		}
-		println!("{}",relative_buf.display());
-	}
 	//println!("{:?}",headerPathList);
 	get_input();
+}
+
+fn make_include_dir(include_dir_path: &PathBuf) {
+    if !include_dir_path.is_dir(){
+			    match fs::create_dir(include_dir_path){
+				    Ok(()) => {
+					    println!("succes create Include dir");
+				    },
+				    Err(e) => {
+					    println!("failed create Include dir {e}");
+					    std::process::exit(1);
+				    },
+			    };
+		    }
+}
+
+fn decided_include_dir(args: &Args, exe_dir_pathbuf: &PathBuf) -> PathBuf {
+	return match args.include_make_dir_path {
+				Some(p) => p,
+				None => exe_dir_pathbuf.join(INCLUDE_DIR_NAME),
+			};
+}
+
+fn get_exe_dir() -> PathBuf {
+    let exe_dir_pathbuf = env::current_dir().unwrap_or_else(|err| {
+		    println!("exe not found path {err}");
+		    std::process::exit(1);
+	    });
+    exe_dir_pathbuf
 }
 
 fn create_all_header(path:&path::PathBuf,write:String,is_first:&mut bool)->std::io::Result<()>{
